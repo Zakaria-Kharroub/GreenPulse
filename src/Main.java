d40f4fd61f481ff581fe6d306d70085c67fa4368
zakaria <kharroubzakaria32@gmail.com>
2024-08-30 10:51:34 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//      -------------------date now
+//        LocalDate thdate = LocalDate.now();
+//        System.out.println( "date now :" + thdate);
+
+//        for (int c=0; c<10;c++){
+//            System.out.println(c);
+//        }
+
+//        float sum =0;
+//        for (float i=0; i<=10; i++){
+//            sum = sum+i;
+//        }
+//        System.out.println(sum);
+
+
+
+    }
+}
